ed7573a81d583908ba0cda7f35345879470ad587
root <root@Mothneps-PC>
2024-09-06 11:20:13 +0200

    Exercise 2 finalized

--- exercise2.hs
+++ exercise2.hs
@@ -3,6 +3,10 @@ import Data.List
 import Data.Char
 import System.Random
 
+{- 
+    Time spent: 120 Minutes
+-}
+
 main :: IO()
 main = do -- <- unwraps a monad and return wraps a monad again
     let nbr = 10000
@@ -13,12 +17,27 @@ main = do -- <- unwraps a monad and return wraps a monad again
     let scdQuart = createQuartile li 0.25 0.50
     let thrdQuart = createQuartile li 0.50 0.75
     let fourthQuart = createQuartile li 0.75 1.00
-    let confLevel = 0.01 --to represent a 99% confidence level
-    -- here we could check if the sum of the amount of values in each quartile is equal to the total given in the beginning
-    print firstQuart
-    print scdQuart
-    print thrdQuart
-    print fourthQuart
+    --to represent a 99% confidence level
+    let lowerConfLev = 2400
+    let upperConfLevel = 2600
+    
+    let checkedFirst = checkQuartile lowerConfLev upperConfLevel firstQuart
+    let checkedscd = checkQuartile lowerConfLev upperConfLevel scdQuart
+    let checkedthrd = checkQuartile lowerConfLev upperConfLevel thrdQuart
+    let checkedfourth = checkQuartile lowerConfLev upperConfLevel fourthQuart
+
+
+{-
+    Based on multiple tests of my functions, assuming we are using 10.000 values to test, a 99% precision can be used to fulfill 
+    our needs. reason being -> never less than 2400 and never more than 2600 values will be summed for a quartile.
+-}
+
+    -- here we could implement a function to check if the sum of the amount of values in each quartile is equal to the total given in the beginning
+    print checkedFirst
+    print checkedscd
+    print checkedthrd
+    print checkedfourth
+    
     
 
 
@@ -33,12 +52,15 @@ probs n = do
 
 
 {-
-    First of all we are going to have a function that sorts our values in the 4 quartiles
-
+    This function uses list comprehension with upper and lower values to
+     categorize every single value in the received list
 -}
 
 createQuartile :: [Float] -> Float -> Float -> Int
 createQuartile li lower upper = length[x| x<-li, x<upper && x>=lower]
+
+checkQuartile :: Int -> Int -> Int -> Bool
+checkQuartile lower upper val = val < upper && val > lower
 
 
     
---
+++ exercise4.hs
@@ -0,0 +1,5 @@
+import Test.QuickCheck
+import Data.List
+import Data.Char
+import System.Random
+
