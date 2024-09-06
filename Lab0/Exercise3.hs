5e368bcf51585d65b923e5b14e4fc8baff6c6430
Merlijn van Uden <merlijn-van-uden@Hotmail.nl>
2024-09-06 17:02:51 +0200

    Exercise3

--- Lab0/Exercise3.hs
+++ Lab0/Exercise3.hs
@@ -3,54 +3,37 @@ import Data.List
 import System.Random
 import Test.QuickCheck
 
--- Exercise 1
--- Time Spent: 35 min
-squareSum1 :: Integer -> Integer
-squareSum1 n = sum [x ^ 2 | x <- [1 .. n]]
-
-squareSum2 :: Integer -> Integer
-squareSum2 n = n * (n + 1) * (2 * n + 1) `div` 6
-
-testSquareSum :: Integer -> Bool
-testSquareSum n = squareSum1 n == squareSum2 n
-
--- main :: IO ()
--- main = quickCheck $ forAll (choose (1, 1000)) testSquareSum
-
--- Exercise 2
--- Time Spent: 210 min
-probs :: Int -> IO [Float]
-probs 0 = return []
-probs n = do
-  p <- getStdRandom random
-  ps <- probs (n - 1)
-  return (p : ps)
-
-createSubsection :: [Float] -> (Float, Float) -> [Float]
-createSubsection list (lower, upper) = [x | x <- list, x >= lower && x < upper]
-
-calculatePercentage :: [Float] -> [Float] -> IO Float
-calculatePercentage list sublist = do
-  return (fromIntegral (length sublist) / fromIntegral (length list))
-
--- chose a range of 0.05, because it works
-withinRange :: [Float] -> Bool
-withinRange = all (\x -> x >= 0.25 - 0.25 * 0.05 && x <= 0.25 + 0.25 * 0.05)
-
-testProbs :: Int -> IO Bool
-testProbs n = do
-  result <- probs n
-  percentageBin1 <- calculatePercentage result (createSubsection result (0, 0.25))
-  percentageBin2 <- calculatePercentage result (createSubsection result (0.25, 0.5))
-  percentageBin3 <- calculatePercentage result (createSubsection result (0.5, 0.75))
-  percentageBin4 <- calculatePercentage result (createSubsection result (0.75, 1))
-
-  return (withinRange [percentageBin1, percentageBin2, percentageBin3, percentageBin4])
-
--- main :: IO ()
--- main = do
---   result <- testProbs 100000
---   print result
-
 -- Excercise 3
--- Time Spent:
+-- Time Spent: 40 min
+
+data Shape = NoTriangle | Equilateral
+   | Isosceles  | Rectangular | Other deriving (Eq,Show)
+
+canFormTriangle :: Integer -> Integer -> Integer -> Bool
+canFormTriangle a b c =
+  a+b > c && b+c > a && c+a > b
+
+triangle :: Integer -> Integer -> Integer -> Shape
+triangle a b c 
+  | not (canFormTriangle a b c) = NoTriangle
+  | a == b && b == c = Equilateral
+  | a == b || a == c || b == c = Isosceles
+  | satisfiesPythagorasTheorem a b c = Rectangular
+  | otherwise = Other
+
+satisfiesPythagorasTheorem :: Integer -> Integer -> Integer -> Bool
+satisfiesPythagorasTheorem a b c
+  | a > b && a > c = a^2 == b^2 + c^2
+  | b > a && b > c = b^2 == a^2 + c^2
+  | otherwise = c^2 == a^2 + b^2
+
+testTriangle :: (Integer, Integer, Integer) -> Shape -> Bool
+testTriangle (a,b,c) shape = triangle a b c == shape
+
+main :: IO ()
+main = do
+  print (testTriangle (3,1,1) NoTriangle)
+  print (testTriangle (1,1,1) Equilateral)
+  print (testTriangle (2,2,1) Isosceles)
+  print (testTriangle (3,4,5) Rectangular)
+  print (testTriangle (3,4,6) Other)
