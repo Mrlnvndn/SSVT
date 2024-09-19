2e8626c060fca66f9e2030639cf69f11abaf92a0
tomapnr <thomaspenner00@gmail.com>
2024-09-19 12:54:47 +0200

    Outstanding Exercise 6

---
+++ Lab1/Exercise6.hs
@@ -0,0 +1,78 @@
+import Data.List
+import System.Random
+import Test.QuickCheck
+
+main :: IO ()
+main = do
+  let f = Impl (Prop 1) (Cnj [Prop 2, Prop 3])
+  print (cnf f)
+
+{-
+Time spent: 120 Minutes
+    We want to tranform formulas in a CNF form. To do so, we need to look at the expression and break it up into
+    clauses (AND or OR) and Literals (Variables). For this particular form we could also look into all the FALSE results of 
+    a truth table, take all its components, negate them and "add" them up together with "OR" and their negated form. Repeat the step
+    with every False line by adding them with "AND"
+    The whole exercise could have been done using a lexer (to tokenize specific cases) and a parser. It would have
+    been clearer and more efficient, but the time limitation hinders us to do such an integration, so we'll stick
+    to identifying cases manually.
+
+    The steps are the following:
+    - Removal of implications and equivalence using De Morgan's law
+    - Moving the negation inside the expressions
+    - Distribute Disjunction on Conjonction
+
+-}
+
+
+type Name = Int
+
+data Form
+  = Prop Name
+  | Neg Form
+  | Cnj [Form]
+  | Dsj [Form]
+  | Impl Form Form
+  | Equiv Form Form
+  deriving (Eq, Ord, Show)
+
+
+
+-- Calling all the following functions
+cnf :: Form -> Form
+cnf = conv . moveNeg . removal 
+
+--Removal of Implications and Equivalences
+removal :: Form -> Form
+removal (Prop x) = Prop x -- Nothing to change
+removal (Neg x) = Neg(removal x) -- Will return the negation of a property with a value that will get assigned to it
+removal (Cnj xs) = Cnj(map removal xs) -- Same Process with mapping on every simple element in the list
+removal (Dsj xs) = Dsj (map removal xs )
+removal (Impl f1 f2) = Dsj[Neg(removal f1),removal f2] -- Apply De morgan's Law (A -> B becomes ~A V B)
+removal (Equiv f1 f2) = Cnj[removal(Impl f1 f2),removal(Impl f2 f1)] --Apply De Morgan's Law (A <-> B becomes (A->B)AND(B->A)), Implication is going to get deconstructed in previous step
+
+--Convert Expressions with negations in front of it and other rules
+moveNeg :: Form -> Form
+moveNeg (Prop x) = Prop x
+moveNeg (Neg (Prop x)) = Neg (Prop x) --Apply negation to normal prop
+moveNeg (Neg(Neg x)) = moveNeg x -- Double negation annulation
+moveNeg (Neg(Cnj xs)) = Dsj (map (moveNeg . Neg) xs) -- ~(A'AND'B) becomes ~A V ~B
+moveNeg (Neg(Dsj xs)) = Cnj (map (moveNeg . Neg) xs) -- ~(A V B) becomes ~A 'AND ~B
+moveNeg (Cnj xs) = Cnj (map moveNeg xs) --Apply negation inside every element
+moveNeg (Dsj xs) = Dsj (map moveNeg xs)
+
+--Distribute Disjunction over Conjunction
+conv :: Form -> Form -- A V (C 'AND' B) becomes (A V B) 'AND' (A V C)
+conv (Cnj fs) = Cnj (map conv fs) -- Nothing to do expect mapping
+conv (Dsj [f1, f2]) = 
+    case (conv f1, conv f2) of
+        (Cnj fs1, f2') -> Cnj (map (\f -> Dsj [f, f2']) fs1)  -- A ∨ (B ∧ C) becomes (A ∨ B) ∧ (A ∨ C)
+        (f1', Cnj fs2) -> Cnj (map (\f -> Dsj [f1', f]) fs2)  -- (A ∧ B) ∨ C becomes (A ∨ C) ∧ (B ∨ C)
+        (f1', f2')     -> Dsj [f1', f2']  -- A ∨ B
+conv (Dsj fs) = Dsj (map conv fs)
+conv f = f
+
+
+
+
+
--- Lab1/Exercise9.hs
+++ Lab1/Exercise9.hs
@@ -2,6 +2,10 @@ import Data.List
 import System.Random
 import Test.QuickCheck
 
+
+--Time Spent: 60 Minutes
+-- Implying code from snippet from lecture
+
 infix 1 --> 
 
 (-->) :: Bool -> Bool -> Bool
@@ -9,38 +13,47 @@ p --> q = (not p) || q
 
 --For a list to be a permutation of another one, it has to be of the same length and none of the elements can be at the same position of the other list
 isPermutation :: Eq a => [a] -> [a] -> Bool
-isPermutation xs ys = iterations xs ys 0 &&  length xs == length ys
+isPermutation xs ys = iterations xs ys  && ys/=xs && length xs == length ys
 
 --Going through my first list, checking wether the current element is part of the second list (has to be) and checking wether it is not at the same index
 --If element holds true for these two conditions, then going to the following element, and iterating through the whole list.
-iterations :: Eq a => [a] -> [a] -> Int -> Bool
-iterations [] _ _ = True
-iterations (x:xs) ys ind
-    | x `elem` ys && ys!!ind /= x = iterations xs ys (ind+1)
-    | otherwise = False
+iterations :: Eq a => [a] -> [a] -> Bool
+iterations xs ys = all (`elem` ys) xs
 
 --If we can assume that there is no duplicates in our list then we can have the following properties to check:
 
---If we order two list of permutations, they should be the same List
+--Property states that 2 permutations of the same list, if ordered, will return the same List
 sameList :: Ord a => [a] -> [a] -> Bool
 sameList xs ys = sort xs == sort ys
 
+--Property states that permutations are symmetric, permutation of a list, then the list is also a permutation of the previous permutation
 symmetry :: Eq a => [a] -> [a] -> Bool
 symmetry xs ys = isPermutation xs ys == isPermutation ys xs
+
+
+-- Generate a random list of 9 integers
+randomList :: IO [Int]
+randomList = sequence [randomIO | _ <- [1..9]]
+
+randomPermGen :: [Int] -> [[Int]]
+randomPermGen xs = permutations xs
+ 
+
+
+
 
 main :: IO()
 main = do
     --List of well chosen Lists to test
-    print (isPermutation [1,2,3] [2,3,1])
-    print (isPermutation [1,2,3,4] [4,3,2,1])
-
-    --Lists that are not permutations
-    print (isPermutation [1,2,3] [3,2,1])
-    print (isPermutation [1,2,3] [2,2,0])
+   randomList' <- randomList
+   print randomList'
+   let randomPerm = randomPermGen randomList'
+   print randomPerm
 
 {-
     You may assume that your input lists do not contain duplicates. What does
     this mean for your testing procedure?
+
     This means that my permutations are easier to test but it wouldn't have altered my testing anyways
 
     It is very complicated to automate the testing process as quickcheck can't easily create permutations. 
