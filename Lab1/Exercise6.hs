eeab2cb7c780cda60b8b0cc92668663ca5604c1c
Merlijn van Uden <merlijn-van-uden@Hotmail.nl>
2024-09-19 15:31:53 +0200

    Finished Exercise6

--- Lab1/Exercise6.hs
+++ Lab1/Exercise6.hs
@@ -1,13 +1,49 @@
 import Lecture3
 
+-- Time Spent: 360 min
+
+-- first remove the arrows, then convert to negation normal form, then apply the distribution laws and finally flatten the logical expression
 cnf :: Form -> Form
-cnf (Equiv f1 f2) = Cnj [Dsj [Neg (cnf f1), cnf f2], Dsj [cnf f1, Neg (cnf f2)]]
-cnf (Impl f1 f2) = Dsj [Neg (cnf f1), cnf f2]
-cnf (Neg (Cnj [f1, f2])) = Dsj [Neg (cnf f1), Neg (cnf f2)]
-cnf (Neg (Dsj [f1, f2])) = Cnj [Neg (cnf f1), Neg (cnf f2)]
-cnf (Neg (Neg f1)) = cnf f1
-cnf (Dsj [f1, Cnj [f2, f3]]) = Cnj [Dsj [cnf f1, cnf f2], Dsj [cnf f1, cnf f3]]
-cnf (Cnj fs) = Cnj (map cnf fs)
-cnf (Dsj fs) = Dsj (map cnf fs)
-cnf (Neg fs) = Neg (cnf fs)
-cnf (Prop f) = Prop f
+cnf fs = flatten $ distlaw $ nnf $ arrowfree fs
+
+distlaw :: Form -> Form
+-- Distribute disjunctions over conjunctions
+distlaw (Dsj fs) = distribute (map distlaw fs)
+-- Traverse through the rest of the Form to distribute disjunctions over conjunctions
+distlaw (Cnj fs) = Cnj (map distlaw fs)
+distlaw (Neg fs) = Neg (distlaw fs)
+distlaw (Prop f) = Prop f
+
+distribute :: [Form] -> Form
+distribute [] = Dsj []
+distribute [f] = f
+-- recursively go over the formulas in the disjunction
+distribute (f : fs) =
+  case f of
+    -- if the head is a conjunction, distribute the formulas in the conjunction over the remainder of the disjunction
+    Cnj gs -> Cnj (map (\g -> distlaw (Dsj (g : fs))) gs)
+    -- if it is not, it will call distribute on the tail of the conjunction
+    _ -> case distribute fs of
+      -- if that results in a conjunction, try to distribute it from the top
+      Cnj gs -> Cnj (map (\g -> distlaw (Dsj [f, g])) gs)
+      -- if it is not a conjunction, create a new disjunction and if possible flatten the tail into it
+      d ->
+        Dsj
+          ( f : case d of
+              Dsj ds -> ds
+              _ -> [d]
+          )
+
+flatten :: Form -> Form
+flatten (Cnj fs) = Cnj (concatMap (flattenCnj . flatten) fs)
+flatten (Dsj fs) = Dsj (concatMap (flattenDsj . flatten) fs)
+flatten (Neg f) = Neg (flatten f)
+flatten (Prop f) = Prop f
+
+flattenCnj :: Form -> [Form]
+flattenCnj (Cnj fs) = concatMap flattenCnj fs
+flattenCnj f = [f]
+
+flattenDsj :: Form -> [Form]
+flattenDsj (Dsj fs) = concatMap flattenDsj fs
+flattenDsj f = [f]
