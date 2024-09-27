e9b3f96806dd52db88a8a4f924c181b9eafaaa0c
Jorrit Kroes <jorrit.kroes@prdct.nl>
2024-09-27 14:45:31 +0200

    Added cool printing and filtering of equivalencies now works

--- Lab2/Exercise3.hs
+++ Lab2/Exercise3.hs
@@ -9,6 +9,8 @@ import Test.QuickCheck
 import FitSpec
 import Control.Monad
 import System.Random
+import Data.List (intercalate)
+import Data.Foldable (find)
 
 -- Time Spent: -- min (14:42)
 {--
@@ -90,16 +92,65 @@ createPropMutantMatrix propMap nMutants fut = do
     return $ Map.fromList (zip (Map.keys propMap) propResults)
 
 
+
+
 -- Calculates Minimal Subset of Properties of a combination of a Function Under Test
 -- and a set of properties
 minPropSubset :: TypeFut -> [Prop] -> [Prop]
 minPropSubset fut props = props
 
 
+isEquiv :: [Bool] -> [Bool] -> Bool
+isEquiv xs ys = xs == ys
+
+mapValues :: Map k v -> [v]
+mapValues m = map (\x -> snd x) (Map.toList m)
+
+
+-- filterEquiv :: Map String [Bool] -> (Map String [Bool], Map String [Bool])
+-- filterEquiv mutationMap = Map.partitionWithKey (\k v -> hasEquiv k v mutationMap) mutationMap where
+--     hasEquiv key val mutationMap = do -- Checks if this mutation result has an equivalent result in the map
+--         let restMap = Map.delete key mutationMap
+--         Map.null $ Map.filter (\x -> isEquiv x val) restMap
+
+filterEquiv :: Map String [Bool] -> (Map String [Bool], Map String (Maybe String))
+filterEquiv mutationMap = foldr partitionEquiv (mutationMap, Map.empty) (Map.keys mutationMap)
+  where
+    partitionEquiv key (uniqueMap, equivMap) =
+      let val = uniqueMap Map.! key
+          restMap = Map.delete key uniqueMap
+          equiv = find (\k -> isEquiv (restMap Map.! k) val) (Map.keys restMap)
+      in case equiv of -- Handle case if equivalence is found or not
+           Just e  -> (restMap, Map.insert key (Just e) equivMap)
+           Nothing -> (uniqueMap, equivMap)
+
+
+printResMap :: Map String [Bool] -> String -> IO()
+printResMap resMap name = do
+    putStrLn $ (show name) ++ ":"
+    putStrLn . (intercalate "\n") $ mapValues $ Map.mapWithKey showEntry resMap
+    putStrLn "\n"
+    where
+    showEntry key x = (show key) ++ ":" ++ show x
+
+printEquivMap :: Map String (Maybe String) -> String -> IO ()
+printEquivMap equivMap name = do
+    putStrLn $ name ++ ":"
+    putStrLn . intercalate "\n" $ map showEntry (Map.toList equivMap)
+    putStrLn "\n"
+  where
+    showEntry (key, Just val) = key ++ " is equivalent to " ++ val
+    showEntry (key, Nothing)  = key ++ " has no equivalent"
+
 main :: IO ()
 main = do
     -- mutations <- getMutants 3
-    -- results <- runProp (properties' !! 1) mutations multiplicationTable
     -- print results
     matrix <- createPropMutantMatrix propMap 5 multiplicationTable
-    print matrix
+    printResMap matrix "propMutantMatrix"
+
+    let (filtered, equiv) = filterEquiv matrix
+    printResMap filtered "After Equiv filter"
+
+    putStrLn $ "Equivalencies found:"
+    printEquivMap equiv "Equivalencies that were found:"
