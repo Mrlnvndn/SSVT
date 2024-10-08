34827ef37a2b7eb5f165edc268dea3c813cf4a28
Jorrit Kroes <jorrit.kroes@prdct.nl>
2024-10-08 11:10:25 +0200

    Tweaked and merged ansers 3-6

--- Lab3/Exercise3.hs
+++ Lab3/Exercise3.hs
@@ -3,23 +3,31 @@ import Data.List
 import System.Random
 import Test.QuickCheck
 import SetOrd ( Set(..), unionSet )
+import Exercise4 (genDomainRelation)
 
 type Rel a = [(a,a)]
 
 {-
-    List comprehension to get the symmetric value of a pair by reorgonising the original tuple, concat to
-    transform the list of lists (originated from the tuple manipulation) to a normal list and nub to get rid of any duplicates
-    (which is the property of a set)
+Exercise 3: Program to get the symmetric closure of relation R.
+
+we use a list comprehension to get the symmetric value of a pair by reorgonising the original tuple. This results
+in a list of symmetric relations.
+We merge the resulting relations by concatenating them together
+Afterwards we get rid of duplicates using 'nub'.
+(Since we can't have duplicates in a Set)
 -}
 symClos :: Ord a => Rel a -> Rel a
 symClos rel = nub $ concat [[(x, y), (y, x)] | (x, y) <- rel]
 
+
 {-
-Example of a set which is going to become a symmetric closure of it. Could be done with a random Set
-but the random generation of sets isn't the same approach for every team member. Therefore to ensure a 
-proper functionnality of this file. A manually prepared set works best.
+Example of a set which is going to become a symmetric closure of it. Also works for randomly generated set.
 -}
 main :: IO()
 main = do
-    let set = [(1,2),(2,3),(3,4)]
-    print $ symClos set
+    let rel = [(1,2),(2,3),(3,4)]
+    print $ symClos rel
+
+    (_, randRelation) <- generate (genDomainRelation)
+    putStrLn $ "Randomly generated relation:" ++ (show randRelation)
+    putStrLn $ "Gives symmetric closure: " ++ (show $ symClos randRelation)
--- Lab3/Exercise6.hs
+++ Lab3/Exercise6.hs
@@ -2,21 +2,35 @@ module Exercise6 where
 import Data.List
 import System.Random
 import Test.QuickCheck
-import SetOrd ( Set(..), unionSet )
+import SetOrd ( Set(..), unionSet, list2set )
 import Exercise3 (symClos)
 import Exercise5 (trClos, trClos',comparePairs)
 
 type Rel a = [(a,a)]
 
-infixr 5 @@
-(@@) :: Eq a => Rel a -> Rel a -> Rel a
-r @@ s = nub [ (x,z) | (x,y) <- r, (w,z) <- s, y == w ]
+-- Time Spent: 60 Minutes
 
 {-  
-    Time Spent: 60 Minutes
-    Following next is a list of well chosen properties for the two functions we put under test. A random test generation with quickcheck is doabe
-    We just need to call QuickCheck on every chosen property. In this specific case we need to specify the type expected in each of the properties.
-    Quickcheck will then generate random Rel variables to be tested.
+Exercise 6: Test symClos and trClos
+
+## Test method
+  Following next is a list of well chosen properties for the two functions we put under test.
+  A random test generation with quickcheck is doable as we just need to call QuickCheck on every chosen property.
+  In this specific case we need to specify the type expected in each of the properties.
+  Quickcheck will then generate random Rel variables to be tested.
+
+## Properties
+We test the following properties for symClos:
+- symmetric closures are symmetric (duh...)
+- Every elements of the relation are also in the symmetric closure
+For trClos:
+- Transitive closures are actually transitive
+- Every element in R is also in transitive closure
+- Idempotence
+
+We test this using some manually crafted relations and their known closures.
+Afterwards we test these using QuickCheck:
+  meaning we generate random relations and check the properties defined.
 -}
 
 -- Property: Symmetry. In a symetrical closure, if we have a pair (a,b) then we should also find (b,a) in the Set
@@ -44,17 +58,18 @@ prop_trClosIdempotent :: Ord a => Rel a -> Bool
 prop_trClosIdempotent r = trR == trClos trR
   where trR = trClos r
 
+
+-- ### Tests ###
+
 -- Manual Tests for each function
-exampleTests :: IO ()
-exampleTests = do
-    print $ symClos [(1,2)] == [(1,2), (2,1)]
-    print $ trClos [(1,2), (2,3)] == [(1,2), (2,3), (1,3)] --Idempotence concept
-
-
+manualTests :: IO ()
+manualTests = do
+    print $ list2set (symClos [(1,2)]) == list2set ([(1,2), (2,1)])
+    print $ list2set (trClos [(1,2), (2,3)]) == list2set [(1,2), (2,3), (1,3)] --Idempotence concept
 
 -- QuickCheck tests with specified types
-runTests :: IO ()
-runTests = do
+runQuickCheckTests :: IO ()
+runQuickCheckTests = do
     quickCheck (prop_symClosSymmetric :: Rel Int -> Bool)
     quickCheck (prop_symClosSuperset :: Rel Int -> Bool)
     quickCheck (prop_trClosTransitive :: Rel Int -> Bool)
@@ -63,6 +78,6 @@ runTests = do
 
 main :: IO ()
 main = do
-    exampleTests
-    runTests
+    manualTests
+    runQuickCheckTests
 
