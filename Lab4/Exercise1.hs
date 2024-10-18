a0e2ec8b9452b120606bf8b7692a5fe41a25824e
Jorrit Kroes <jorrit.kroes@prdct.nl>
2024-10-18 12:47:50 +0200

    Add / merge all exercises and make uniform

--- Lab4/Exercise1.hs
+++ Lab4/Exercise1.hs
@@ -1,1 +1,133 @@
+module Exercise1 where
 
+import LTS
+import Data.List (intersect, subsequences, union)
+import Test.FitSpec ( property, (==>), Property )
+import Data.Kind (Type)
+
+-- Time Spent: 120 min
+
+{-
+Ex1: IOLTS validation -> ".. any verification is only as good as the validity of the model"
+
+Q1: What makes an IOLTS 'invalid'
+
+According to the definition by Tretmans et al.:
+    From definition of LTS:
+    1. Set of states Q is: countable and non-empty
+    2. special symbol tau cannot be in Transition relation T
+    3. transition relation T ⊆ Q × (L ∪ {τ }) × Q (all relations can be built from the cartesian product of
+        all combinations of 2 states and all labels (+ tau).
+    4. Initial state q_0 should of course be present in Q
+
+    Additionally from definition of IOLTS
+    1. L_I and L_U are 'countable sets' (p.13)
+    2. L_I ∩ L_U = ∅ (empty set), meaning input 'external actions' are not also output (/system initiated) actions.
+
+This gives us the following conditions that lead to an invalid IOLTS
+
+### Note:
+Checking that states and labellists are countable is tricky, because inifinite lists are common in Haskell.
+Testing for this results in the Halting Problem so we cover those cases by simply counting the length
+and accepting the function will never finish if this condition is not met.
+
+### Conditions that invalidate an IOLTS
+Apart from that we get conditions:
+1. Set of states is empty
+2. tau is in L_in
+3. for any relation in T (q1, l, q2): q1 not in Q, l not in L or q2 not in Q (q1 is allowed to be equal to q2)
+4. q_0 is not in Q
+5. There is at least one label in both input and output set
+
+
+Q2: validateLTS implementation
+
+We wrote a function to calculate the possible transitions by calculating the cartesian product between
+all states and combining this with all labels in 'possibleTransitions' so we get all possible transition triples.
+This is required to test #3.
+Then all conditions that invalidate an IOLTS were converted to code. If None is found we return True and an IOLTS
+is deemed valid.
+
+Q3: Create properties for validateLTS
+
+We created properties from the same definitions of a valid IOLTS, but they are defined the other way around:
+    i.e. we emptyLabelsIntersect to test: if intersection l_in and l_out is null then we expect the model to be valid
+        and if the model is 'valid' then the prop HAS to return true.
+They are 'the other way around' in the sense that they test the positive version instead of 'invalidating' when something
+out of line is found.
+
+
+-}
+
+
+-- Checks countability by counting x :) (will loop forever if an infinite list is encountered)
+countable ::forall (t :: Type -> Type) a. Foldable t => t a -> Bool
+countable x = length x `seq` True
+
+
+-- ## Main implementation of model validation ##
+cartProd :: Ord a => [a] -> [a] -> [(a,a)]
+cartProd xs ys = [(x,y) | x <- xs, y <- ys]
+
+-- Calculates all possible transitions given the domain of states and labels
+possibleTransitions :: [State] -> [Label] -> [LabeledTransition]
+possibleTransitions states labels = [(fst stateTrans, label, snd stateTrans)
+                | stateTrans <- cartProd states states, label <- labels]
+
+
+-- Function that validates an IOLTS using the definition by Tretmans
+validateLTS :: IOLTS -> Bool
+validateLTS ([], _, _, _, _) = False
+validateLTS (states, l_in, l_out, transitions, q_0)
+    | (not (countable states) || not (countable l_in) || not (countable l_out) ) = False
+    | (not $ q_0 `elem` states) = False
+    | tau `elem` l_in = False
+    | l_in `intersect` l_out /= [] = False
+    | any (\t -> not $ transitionPossible t states (l_in `union` l_out ++ [tau, delta])) transitions = False
+    | otherwise = True
+        where
+        transitionPossible transition states labels = 
+            transition `elem` (possibleTransitions states labels)
+
+
+-- Helper function to define how truth value of property and a validation should relate
+-- We cannot just do ==, because there could be another part of IOLTS that invalidates the model.
+-- So this essentially means: if model is valid the prop should pass. If the prop does not pass then the model
+-- should be found to be invalid. Anything inbetween we are not sure about so we let it pass as well.
+holdsForProp :: IOLTS -> Bool -> Bool
+holdsForProp model prop = validation ==> prop && not prop ==> not validation
+    where validation = validateLTS model
+
+-- ## Properties ## (explanation above)
+prop_emptyLabelsIntersect :: IOLTS -> Property
+prop_emptyLabelsIntersect model@(_, l_in, l_out, _, _) = property $
+    holdsForProp model (null $ l_in `intersect` l_out)
+
+prop_countable :: IOLTS -> Property
+prop_countable model@(states, l_in, l_out, _, _) = property $ 
+    holdsForProp model $ countable states && countable l_in && countable l_out
+
+prop_transitionsExist :: IOLTS -> Property
+prop_transitionsExist model@(states, l_in, l_out, transitions, _) = property $
+    holdsForProp model transitionsExist
+    where
+        transitionsExist =
+            all
+            (\t -> t `elem` (possibleTransitions states (l_in `union` l_out ++ [tau, delta])))
+            transitions
+
+
+
+-- Make list of IOLTS models in LTS.hs
+ltsList :: [IOLTS]
+ltsList = [ counterImpl, counterModel, coffeeImpl1, coffeeModel1, coffeeImpl2, coffeeModel2, coffeeImpl3,
+    coffeeModel3, coffeeImpl4, coffeeModel4, coffeeImpl5, coffeeModel5, coffeeImpl6,coffeeModel6,
+    tretmanK2,tretmanK3,
+    tretmanI1,tretmanI2,tretmanI3,tretmanI4,
+    tretmanS1,tretmanS2,tretmanS3, tretmanS4,
+    tretmanR1,tretmanR2]
+
+main :: IO ()
+main = do
+    -- Validate all iolts models
+    mapM_ putStrLn (map (\lts -> if validateLTS lts then "Validated" else "Invalid model") ltsList)
--- Lab4/Exercise2.hs
+++ Lab4/Exercise2.hs
@@ -1,1 +1,108 @@
+module Exercise2 where
+
+import Data.List (intersect)
+import LTS (createIOLTS, State, Label, LabeledTransition, IOLTS)
+import Test.QuickCheck
+import System.Random
+import Control.Monad (replicateM)
+
+-- Time spent: 120 minutes 
+
+
+{-
+Generate random char for the strings,
+random int for the integer
+but need to decided and give proof for ! or ? on the state
+
+According to Tretmans [2008] - Model-Based Testing with LTS, a valid IOLTS satisfies the following (and more):
+1. Input Enabledness:
+    The system should be input-enabled, meaning it must handle every possible input at every state.
+2. Quiescence
+    Represents a state where the system is not producing any outputs. Quiescence can be treated 
+    as a special output observable by the tester. The IOLTS must explicitly represent quiescence 
+    transitions to allow testing systems that may enter a state of waiting for further inputs 
+    without producing any outputs.
+3. Determinism
+    IOLTS are often expected to be deterministic when used in testing. This means that for 
+    any given input, the system should have a unique transition to a next state
+4. Finite-State and Convergence
+    Systems are often modeled as finite-state to make them practically testable and analyzable. 
+    Finite-state systems have a limited number of states and transitions, ensuring the possibility 
+    of running exhaustive tests.
+    Strong Convergence may also be required, which ensures that the system does not engage in 
+    infinite sequences of internal actions (τ-actions).
+
+However, this random IOLTS generator will be used to generate IOLTS to pass through the properties produced in Exercise1.
+So, the reason for developing properties is verify that IOLTS satifies those properties. Therefore, it would not make sense
+to make a random IOLTS to generate valid IOLTS's that satisfy the properties. 
+
+-}
+
+-- type State = Integer
+-- type Label = String
+-- type LabeledTransition = (State, Label, State)
+-- type IOLTS = ([State], [Label], [Label], [LabeledTransition], States
+ 
+
+{-
+Loop through random list of size n
+add (randInt, randLabel, randInt) and repeat for whole list 
+-}
+
+-- Generate a random character
+randChar :: Gen Char
+randChar = choose ('a', 'z')
+
+-- Generate a random label
+generateRandomLabel :: Gen Label
+generateRandomLabel = do
+    isInput <- choose (1 :: Int, 2 :: Int)
+    ioLabel <- randChar
+    if isInput == 1
+        then return ('?' : [ioLabel])
+        else return ('!' : [ioLabel])
+
+-- Generate a random state
+genRandomState :: Integer -> Gen Integer
+genRandomState n = choose (0, n)
+
+-- Generate a random transition
+generateTransitions :: Gen (Integer, Label, Integer)
+generateTransitions = do
+    n <- choose (0, 10)
+    fromState <- genRandomState n
+    label <- generateRandomLabel
+    toState <- genRandomState n
+    return (fromState, label, toState)
+
+-- Generate a random IOLTS
+ltsGen :: Gen IOLTS
+ltsGen = do 
+    len <- choose (1, 20)  -- Length of the list of transitions
+    transitions <- replicateM len generateTransitions
+    return $ createIOLTS transitions
+
+
+-- Properties from Exercise 1
+prop_emptyLabelsIntersect :: IOLTS -> Property
+prop_emptyLabelsIntersect (_, l_in, l_out, _, _) = 
+    property $ null $ l_in `intersect` l_out
+
+prop_countable :: IOLTS -> Property
+prop_countable (states, l_in, l_out, _, _) = 
+    property $ countable states .&&. countable l_in .&&. countable l_out
+  where
+    countable :: [a] -> Property
+    countable = property . const True  -- Placeholder, replace with actual countable check
+
+prop_transitionsExist :: IOLTS -> Property
+prop_transitionsExist (_, _, _, transitions, _) = 
+    property $ not (null transitions)
+
+main :: IO ()
+main = do
+    quickCheck (forAll ltsGen prop_emptyLabelsIntersect)
+    quickCheck (forAll ltsGen prop_countable)
+    quickCheck (forAll ltsGen prop_transitionsExist)
+
 
